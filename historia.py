623d34de9267027cfd5f58fce7e4578019badc15
Matheus <145228073+MatheusLuizTdSilva@users.noreply.github.com>
2025-03-22 17:15:28 -0300

    Add files via upload

---
+++ historia.py
@@ -0,0 +1,6 @@
+Nome = input("Insira o nome: ")
+Idade = int(input("Insira a idade de "+ Nome +": "))
+Esitlo = input(Nome+" qual seu gosto musical? :")
+Instrumento = input(Nome+" diga um instrumento para tocar: ")
+Banda = input (Nome+" diga um nome para sua banda: ")
+print(f"{Nome} no seu auge dos {Idade} anos, curtia muito {Esitlo} \nentão decidiu se juntar con uns amigos e criar a {Banda}!!\né incrível de ver eles tocando!! \n{Instrumento} que {Nome} toca é muito f@da!!")
